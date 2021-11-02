416450d5ba393c095e6a840c2e6f89eb455fbf54
Emmanuel Izilov <em5750@gmail.com>
2021-11-01 23:28:26 -0400

    initial commit

---
+++ home.js
@@ -0,0 +1,8 @@
+
+const dnaAnimation = document.querySelector('loader')
+
+dnaAnimation.addEventListener('click', pauseAnimation)
+
+function pauseAnimation(){
+    element.style.animationPlayState = 'paused';
+}
